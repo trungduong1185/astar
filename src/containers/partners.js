e49729c534d39181ebee307c78e8994987ed5f95
trungduong1185 <trungduong1185@gmail.com>
2025-08-01 14:37:26 +0700

    Initial commit from local source

---
+++ src/containers/partners.js
@@ -0,0 +1,67 @@
+import '../css/tachyons.min.css';
+import '../css/styles.css';
+
+const Partners = () => (
+  <>
+    <div className="w-100 pv3">
+      <div className="container-1400 tc center">
+        <p className="_partner-title f2 fw7 tracked pv4 mb2">PARTNERS</p>
+        <div className="pv3 flex flex-wrap justify-center items-center list-partner-element">
+        
+          <a 
+            href="https://deepmind.com/" 
+            className="partner-element" 
+            target="_blank" 
+            title="Google DeepMind - AI Research"
+          >
+            <img
+              alt="Google DeepMind - AI Research"
+              src="images/partners/google-deepmind.svg"
+              className="accordion featured"
+            />
+          </a>
+          
+          
+          
+          <a 
+            href="https://virx.stanford.edu/" 
+            className="partner-element" 
+            target="_blank" 
+            title="ViRx@STANFORD - Virus Treatment (Rx)"
+          >
+            <img
+              alt="ViRx@STANFORD - Virus Treatment (Rx)"
+              src="images/partners/virx.svg"
+              className="accordion featured"
+            />
+          </a>
+          <a 
+            href="https://elsaspeak.com/" 
+            className="partner-element" 
+            target="_blank" 
+            title="ELSA Speak - English Learning"
+          >
+            <img
+              alt="ELSA Speak - English Learning"
+              src="images/partners/elsa.svg"
+              className="accordion featured"
+            />
+          </a>
+          <a 
+            href="https://vietai.org/" 
+            className="partner-element" 
+            target="_blank" 
+            title="VietAI - Vietnamese AI Research Community"
+          >
+            <img
+              alt="VietAI - Vietnamese AI Research Community"
+              src="images/partners/vietai.svg"
+              className="accordion featured"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </>
+);
+export default Partners;
